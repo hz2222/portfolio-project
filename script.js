7f0b67ec6dd57facd9400c527fd8faeb8e0607a7
hz <lebber@gmail.com>
2022-09-01 17:33:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function displayp1() {
+    var paragraph1 = document.querySelector("#p1");
+    paragraph1.innerHTML = "HTML code ensures the proper formatting of text and images for your Internet browser. Without Html, a browser would not know how to display text as elements or load images or other elements. ";
+}
+
+function displayp2() {
+    var paragraph2 = document.querySelector("#p2");
+    paragraph2.innerHTML = "CSS stands for Cascading Style Sheet, it is a style sheet language used to shape the HTML elements that will be displayed in the browsers as a web-page.";
+}
+
+function displayp3() {
+    var paragraph3 = document.querySelector("#p3");
+    paragraph3.innerHTML = "Javascript is used by programmers across the world to create dynamic and interactive web content like applications and browsers. JavaScript is so popular that it's the most used programming language in the world, used as a client-side programming language by 97.0% of all websites.";
+}
